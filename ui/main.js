5ce29b67da7d1ce9e8c99e3c64ae27cab6089540
kiruthikanirmal1 <kiruthikanimi@gmail.com>
2016-12-01 21:52:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,1 @@
 console.log('check!');
-function Change(node){
-
- var j=document.getElementsByClassName("on");
-  for(var i=0;i<j.length;i++){
-    j[i].className="";
-    }  
-  node.className="on";  
-
-    }
