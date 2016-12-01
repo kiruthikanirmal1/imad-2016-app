e008e7c2314b789f87eb4556dceda5139f5cdd14
kiruthikanirmal1 <kiruthikanimi@gmail.com>
2016-12-01 21:24:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('check!');
+function changeBackground() {
+        if (sessionStorage.getItem('colour')) {
+            document.body.style.backgroundColor = sessionStorage.getItem('colour');
+        }else{
+            document.body.style.backgroundColor =  "#BB0A21";
+            sessionStorage.setItem('colour', "#BB0A21");
+        }
+    }
+
+  // then you'll need to call your function on page load
+  changeBackground();
