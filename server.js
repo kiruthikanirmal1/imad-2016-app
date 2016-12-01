113f8f53c7b9e29db9ebb83a6067d3a9daef6a73
kiruthikanirmal1 <kiruthikanimi@gmail.com>
2016-12-01 21:58:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/color"});
   response.write("Hello My Webapp");
   response.end();
 }).listen(8080);
