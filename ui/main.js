1c2e098530cee6fc6c4aa63b8d8e81d6a0118db3
kiruthikanirmal1 <kiruthikanimi@gmail.com>
2016-12-02 19:16:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,6 @@ function myFunction()
     document.getElementById('foo').setAttribute("Hello My Webapp", "style1");
 
     document.body.style.backgroundColor = "red";
+    document.getElementById("demo").setAttribute(
+   "style", "font-size: 100px; font-style: italic; color:#ff0000;");
 }
